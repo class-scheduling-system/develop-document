20c8e351023492d180fc68b86b69db48167b6dc1
XiaoLFeng <gm@x-lf.cn>
2025-04-12 17:30:25 +0800

    docs: 更新 mkdocs.yml 和 KaTeX 相关 JavaScript 文件
    - 在 mkdocs.yml 中新增了 KaTeX 的 CDN 引用，增强了数学公式渲染的能力。
    - 更新了 katex.js 文件，替换了原有的 MathJax 渲染逻辑，改为使用 renderMathInElement 方法，提升了公式渲染的灵活性和性能。

--- docs/assets/js/katex.js
+++ docs/assets/js/katex.js
@@ -1,19 +1,10 @@
-window.MathJax = {
-  tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
-    processEscapes: true,
-    processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
-  }
-};
-
-document$.subscribe(() => { 
-  MathJax.startup.output.clearCache()
-  MathJax.typesetClear()
-  MathJax.texReset()
-  MathJax.typesetPromise()
+document$.subscribe(({ body }) => { 
+  renderMathInElement(body, {
+    delimiters: [
+      { left: "$$",  right: "$$",  display: true },
+      { left: "$",   right: "$",   display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true }
+    ],
+  })
 })
